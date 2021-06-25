9cdb04525fa70d19b646e1b722244785142eafb3
Desire Kaleba <desirekaleba@gmail.com>
2021-06-25 14:47:01 +0300

    Add hello RUST

---
+++ hello_rust/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    read();
+    write();
+    increment();
+}
+
+fn read() {
+    println!("Reading");
+}
+
+fn write() {
+    println!("Writing");
+}
+
+fn increment() {
+    println!("+1");
+}

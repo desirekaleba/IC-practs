30c314853932e007ea1f4dff3e07296ef21caf5b
Desire Kaleba <desirekaleba@gmail.com>
2021-06-25 14:49:45 +0300

    Add info file

---
+++ hello_rust/target/rls/debug/deps/hello_rust-0c4751338b505068.d
@@ -0,0 +1,5 @@
+/media/desire/deska/deska/prog/internet_computer/hello_rust/target/rls/debug/deps/hello_rust-0c4751338b505068.rmeta: src/main.rs
+
+/media/desire/deska/deska/prog/internet_computer/hello_rust/target/rls/debug/deps/hello_rust-0c4751338b505068.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/rls/debug/deps/hello_rust-8d460ec7908d4baf.d
@@ -0,0 +1,5 @@
+/media/desire/deska/deska/prog/internet_computer/hello_rust/target/rls/debug/deps/hello_rust-8d460ec7908d4baf.rmeta: src/main.rs
+
+/media/desire/deska/deska/prog/internet_computer/hello_rust/target/rls/debug/deps/hello_rust-8d460ec7908d4baf.d: src/main.rs
+
+src/main.rs:
